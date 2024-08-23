313a7bac1b8d6b4986e80afa4100495f0a0c6b4b
Kevin Altmeier <kevinaltmeier@student.unsw.edu.au>
2024-08-24 00:49:07 +1000

    Starting work on a personal Gym tracking app

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LocalDate lc = LocalDate.parse("2024-08-24");
+        Workout workout = new Workout(lc, "Bench Press");
+
+        System.out.println(workout.getDateEntered());
+        System.out.println(workout.getWorkoutName());
+
     }
 }
--- src/Workout.java
+++ src/Workout.java
@@ -1,11 +1,30 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Workout {
-    private LocalDate dateEntered = LocalDate.now();
+    private LocalDate dateEntered;
     private String workoutName;
+    private List<WorkoutType> workoutType = new ArrayList<WorkoutType>();
 
     public Workout(LocalDate dateEntered, String workoutName) {
         this.dateEntered = dateEntered;
+        this.workoutName = workoutName;
+    }
+
+    public LocalDate getDateEntered() {
+        return dateEntered;
+    }
+
+    public void setDateEntered(LocalDate dateEntered) {
+        this.dateEntered = dateEntered;
+    }
+
+    public String getWorkoutName() {
+        return workoutName;
+    }
+
+    public void setWorkoutName(String workoutName) {
         this.workoutName = workoutName;
     }
 }
